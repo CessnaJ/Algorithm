e7b62af4b4cb35b047df432e8e66f99d789a869b
CessnaJ <eon8748@gmail.com>
2022-09-29 03:53:32 +0900

    recursive

--- SSAFY. 연산.py
+++ SSAFY. 연산.py
@@ -1,17 +1,30 @@
-Vertex, Edges = map(int, input().split()) # 얘는 똑같고
-keys = list(range(Vertex+1))
+from collections import deque
 
+T = int(input())
 
-new_dict = dict.fromkeys(keys)
+for case in range(1, T+1):
+    start, result = map(int, input().split())
+    calc_set = set()
+    calc_set.add(start)
+    queue = deque()
+    queue.append(start)
+    count = 0
+    while result not in calc_set:
+        for _ in range(len(queue)):
+            current = queue.popleft()
+            if current+1 not in calc_set and 1 <= current+1 <= 1000000:
+                calc_set.add(current+1)
+                queue.append(current+1)
+            if current-1 not in calc_set and 1 <= current-1 <= 1000000:
+                calc_set.add(current-1)
+                queue.append(current-1)
+            if current*2 not in calc_set and 1 <= current*2 <= 1000000:
+                calc_set.add(current*2)
+                queue.append(current*2)
+            if current-10 not in calc_set and 1 <= current-10 <= 1000000:
+                calc_set.add(current-10)
+                queue.append(current-10)
+        count += 1
 
-for key in new_dict:
-    new_dict[key] = []
-
-
-new_dict[3].append(5)
-print(new_dict)
-
-
-# from collections import defaultdict
-
+    print(f'#{case} {count}')
 
