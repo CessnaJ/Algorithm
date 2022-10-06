ea9ad082338ed01357213962f5e4c0443755605a
CessnaJ <eon8748@gmail.com>
2022-10-07 08:24:04 +0900

    temp

--- 토마토 tset.py
+++ 토마토 tset.py
@@ -1,8 +1,5 @@
-# 델타탐색
-# 확장성, 재사용성에 대한 고민
-# 3차원이상의 배열이면 접근할때 고차원 -> 저차원으로 간다는것.
-
 import sys
+input = sys.stdin.readline
 from collections import deque
 length, width, height = map(int, input().split()) # 2 ≤ M ≤ 100, 2 ≤ N ≤ 100, 1 ≤ H ≤ 100
 # 1 - 익은 토마토
@@ -14,36 +11,25 @@ dx = [0, 0, 0, 0, 1, -1]
 dy = [0, 0, 1, -1, 0, 0]
 
 def tomato_check():
-    # global count
+    global count
     for z in range(height):
         for y in range(width):
             for x in range(length):
                 if matrix[z][y][x] == 0:
-                    print(-1)
-                    # return
-                    exit()
+                    count = -1
+                    return
 
-# def isvalid(newZ, newY, newX):
-#     if not (0 <= newX < length and 0 <= newY < width and 0 <= newZ < height):
-#         return False
-#
-#     if visited[newZ][newY][newX]:
-#         return False
-#
-#     if matrix[newZ][newY][newX] in [-1, 1]:
-#         return False
-#     return True
 
-def isvalid(newZ, newY, newX):
-    if (0 <= newX < length and 0 <= newY < width and 0 <= newZ < height) and matrix[newZ][newY][newX] == 0:
-        return True
-    else:
+def isvalid(newZ, newY, newX):                                                # 유효성 검사
+    if not (0 <= newX < length and 0 <= newY < width and 0 <= newZ < height): # 경계 침범시 False
         return False
 
-# sys.stdin.readline().strip()
+    if matrix[newZ][newY][newX] in [-1, 1]:                                   # 썩으면 막히고, 이미 익었으면 중복호출없도록 False
+        return False
+    return True
+
+
 matrix = [[list(map(int, sys.stdin.readline().split())) for y in range(width)] for z in range(height)]
-# print(matrix)
-visited = [[[False]*length for yy in range(width)] for zz in range(height)]
 
 Q = deque()
 for z in range(height):
@@ -55,8 +41,6 @@ for z in range(height):
 while Q:
     z, y, x, count = Q.popleft()
     matrix[z][y][x] = 1
-
-    if isvalid(z, y, x):
         for dir in range(6):
             tempz = z + dz[dir]
             tempy = y + dy[dir]
@@ -64,6 +48,5 @@ while Q:
             if isvalid(tempz, tempy, tempx):
                 Q.append((tempz, tempy, tempx, count+1))
 
-tomato_check()
+tomato_check() # 0이 있으면 count변수를 -1로 바꿔주는 함수. 0을 발견하면 return을 해서 끝내기.
 print(count)
-
