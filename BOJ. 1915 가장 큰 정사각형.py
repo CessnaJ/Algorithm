ff654c3eb9704d2a5e307c302ae9ed20b77bfb4b
CessnaJ <eon8748@gmail.com>
2022-11-01 08:15:35 +0900

    study

--- BOJ. 1915 가장 큰 정사각형.py
+++ BOJ. 1915 가장 큰 정사각형.py
@@ -16,20 +16,22 @@ for row in range(rows):      # 기준 row
         if matrix[row][col]: # 기준좌표에서 시작하겠다.
             cycle = 1        # 시작할때 기본 사이클값
             while cycle:     # 사이클이 0이 아니라면 반복
-                for i in range(cycle+1): # 사이클
-                    if row+i < rows and col+i < cols and matrix[row+cycle-1][col+i] and matrix[row+i][col+cycle-1]:
-                        pass
+                if row+cycle < rows and col+cycle < cols:
+                    for i in range(cycle+1): # 0~사이클 (사이클+1개)
+                        if row+i < rows and col+i < cols and matrix[row+cycle][col+i] and matrix[row+i][col+cycle]:
+                            #                                 기준+사이클로 고정, 기준+0~사이클까지 가변
+                            pass
+                        else:
+                            dp_matrix[row][col] = cycle+0
+                            cycle = 0
+                            break
                     else:
-                        dp_matrix[row][col] = cycle+0
-                        cycle = 0
-                        break
-                else:
-                    cycle += 1
+                        cycle += 1
 
 max_size = 0
 for i in range(rows):
     for j in range(cols):
         max_size = max(max_size, dp_matrix[i][j])
 
-print(*dp_matrix, sep='\n')
+# print(*dp_matrix, sep='\n')
 print(max_size**2)
