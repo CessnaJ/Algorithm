fec3a575130fc4d509b10d6c77f9e95d0df7cbcd
CessnaJ <eon8748@gmail.com>
2022-10-31 18:03:10 +0900

    dp

--- BOJ. 1915 가장 큰 정사각형.py
+++ BOJ. 1915 가장 큰 정사각형.py
@@ -5,5 +5,31 @@ from sys import stdin
 
 rows, cols = map(int, input().split())
 
-matrix = [map(int, stdin.readline().split()) for _ in range(rows)]
+matrix = [list(map(int, list(stdin.readline().strip()))) for _ in range(rows)]
+zip_matrix = list(zip(*matrix))
 
+
+dp_matrix = [[0]*cols for _ in range(rows)]
+
+for row in range(rows):      # 기준 row
+    for col in range(cols):  # 기준 col
+        if matrix[row][col]: # 기준좌표에서 시작하겠다.
+            cycle = 1        # 시작할때 기본 사이클값
+            while cycle:     # 사이클이 0이 아니라면 반복
+                for i in range(cycle+1): # 사이클
+                    if row+i < rows and col+i < cols and matrix[row+cycle-1][col+i] and matrix[row+i][col+cycle-1]:
+                        pass
+                    else:
+                        dp_matrix[row][col] = cycle+0
+                        cycle = 0
+                        break
+                else:
+                    cycle += 1
+
+max_size = 0
+for i in range(rows):
+    for j in range(cols):
+        max_size = max(max_size, dp_matrix[i][j])
+
+print(*dp_matrix, sep='\n')
+print(max_size**2)
