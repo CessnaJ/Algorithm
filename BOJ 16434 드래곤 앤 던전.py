4ca230ece426df6df4fe3a732c493f92afb7b3a4
CessnaJ <eon8748@gmail.com>
2022-11-07 08:07:13 +0900

    asd

--- BOJ 16434 드래곤 앤 던전.py
+++ BOJ 16434 드래곤 앤 던전.py
@@ -1,1 +1,19 @@
-pass
+from sys import stdin
+damage = 0
+max_damage = 0
+rooms, myatk = map(int, stdin.readline().split())
+for _ in range(rooms):
+    which, attack, hp = map(int, stdin.readline().split())
+    if which == 1:
+        if hp % myatk: # 딱 떨어진다.
+            num_of_attacked = hp // myatk
+        else: # 딱 안 떨어진다.
+            num_of_attacked = hp // myatk - 1
+        damage += (attack * num_of_attacked)
+        max_damage = max(damage, max_damage)
+    else:
+        myatk += attack
+        damage -= hp
+        if damage < 0:
+            damage = 0
+print(max(max_damage, damage) + 1)
